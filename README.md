d8fdad3a0b7f11d073a9996fc1f7bd514bd1d839
Gio d'Amelio <giodamelio@gmail.com>
2015-12-11 17:50:59 -0800

    Add airbnb style guide and update source to follow it

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A react project boilerplate. Based on [@gaeron's](https://github.com/gaearon) [react-transform-boilerplate](https://github.com/gaearon/react-transform-boilerplate).
 
+## Features
+
+ - [Follows Airbnb's Javascript Style Guide](https://github.com/airbnb/javascript)
+
 ## Installation
 
 ```bash
---
+++ src/Counter.js
@@ -0,0 +1,32 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class Counter extends Component {
+  static propTypes = {
+    color: PropTypes.string.isRequired,
+    increment: PropTypes.number.isRequired,
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  tick() {
+    this.setState({
+      counter: this.state.counter + this.props.increment,
+    });
+  }
+
+  render() {
+    return (
+      <h1 style={{ color: this.props.color }}>
+        Counter ({this.props.increment}): {this.state.counter}
+      </h1>
+    );
+  }
+}
